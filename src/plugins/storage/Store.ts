24d595931c02020e6c3edaf553d2a50d15190f90
jafayer <github@fayer.me>
2024-10-14 17:56:53 -0400

    Make the store definition allow synchronous lookups

--- src/plugins/storage/Store.ts
+++ src/plugins/storage/Store.ts
@@ -1,6 +1,7 @@
 import { Handler } from '../../common/server';
 import { ZoneData, SupportedRecordType } from '../../types/dns';
 import { EventEmitter } from 'events';
+import { Awaitable } from '../../common/core/utils';
 
 export abstract class Store extends EventEmitter {
   /**
@@ -11,7 +12,7 @@ export abstract class Store extends EventEmitter {
   abstract get<T extends SupportedRecordType>(
     zone: string,
     rType?: T,
-  ): Promise<ZoneData[T][] | ZoneData[keyof ZoneData][] | null>;
+  ): Awaitable<ZoneData[T][] | ZoneData[keyof ZoneData][] | null>;
 
   /**
    * Set or update information about a zone in the database.
@@ -19,7 +20,7 @@ export abstract class Store extends EventEmitter {
    * @param rType The record type to set
    * @param data The data to set
    */
-  abstract set<T extends SupportedRecordType>(zone: string, rType: T, data: ZoneData[T] | ZoneData[T][]): Promise<void>;
+  abstract set<T extends SupportedRecordType>(zone: string, rType: T, data: ZoneData[T] | ZoneData[T][]): Awaitable<void>;
 
   /**
    * Append information about a zone in the database.
@@ -27,7 +28,7 @@ export abstract class Store extends EventEmitter {
    * @param rType The record type to append
    * @param data The data to append
    */
-  abstract append<T extends SupportedRecordType>(zone: string, rType: T, data: ZoneData[T]): Promise<void>;
+  abstract append<T extends SupportedRecordType>(zone: string, rType: T, data: ZoneData[T]): Awaitable<void>;
 
   /**
    *
@@ -35,7 +36,7 @@ export abstract class Store extends EventEmitter {
    * @param rType the record type to delete. If not provided, all records in the zone should be deleted.
    * @param data the data to delete. If not provided, all records of the given type should be deleted.
    */
-  abstract delete<T extends SupportedRecordType>(zone: string, rType?: T, rData?: ZoneData[T]): Promise<void>;
+  abstract delete<T extends SupportedRecordType>(zone: string, rType?: T, rData?: ZoneData[T]): Awaitable<void>;
 
   abstract handler: Handler;
 }
