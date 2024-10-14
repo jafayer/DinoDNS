65cd36e5e14b921f1960f461ebc9d0da9c66a08a
Josh Fayer <github@fayer.me>
2024-10-14 18:06:42 -0400

    Alter storage definitions (#13)
    * Add generic trie implementation
    
    * Update preliminary generic trie implementation details
    
    * Modify store definition to use Data instead of Answer
    
    * Implement new defaultstore
    
    * Create supporting types and utilize new definitions
    
    * Enhance typing of store, but a little shakily
    
    * Fix formatting and linting bugs
    
    * Improve typing of trie, store, and defaultstore
    
    * Format
    
    * Update types of test that used store
    
    * Updated store handler usage to bind to the class
    
    * Bind the store handler in the constructor
    
    * Format
    
    * Fix trie bugs and add better unit tests
    
    * Format
    
    * Create MapStore in anticipation of replacing triestore as default store
    
    * Format
    
    * Add comment and bring trie up to date
    
    * Add compatible toString and fromString methods
    
    * Rename mapstore to defaultstore
    
    * Make the store definition allow synchronous lookups
    
    * Delete outdated triestore tests
    
    * Make the mapstore synchronous  and add handler bind
    
    * format
    
    * Remove unused parameter

--- src/plugins/storage/DefaultStore/index.ts
+++ src/plugins/storage/DefaultStore/index.ts
@@ -1,323 +1,171 @@
+import { ZoneData, SupportedRecordType, SupportedAnswer } from '../../../types/dns';
 import { Store } from '../Store';
 import { EventEmitter } from 'events';
-import { DNSRequest, DNSResponse, NextFunction, Handler } from '../../../common/server';
-import { resolveWildcards } from '../../../common/core/domainToRegexp';
-import { RecordType } from 'dns-packet';
-import { SupportedAnswer } from '../../../types/dns';
+import { DNSRequest, DNSResponse, NextFunction } from '../../../types/server';
 
-interface DeserializedTrieData {
-  trie: DeserializedTrie;
-  data: [string, SupportedAnswer[]][];
-}
+type TypedMap<K extends keyof T, T> = Map<K, T[K][]>;
 
-interface DeserializedTrie {
-  [key: string]: DeserializedTrieData;
-}
-/**
- * A Trie data structure for storing zone data in memory.
- *
- * The trie should be able to resolve data for a given domain by traversing the trie of
- * inverse-ordered domain segment labels.
- *
- */
-export class AnswerTrie {
-  private trie: Map<string, AnswerTrie> = new Map();
-  /**
-   * TODO: this type definition is currently imprecise.
-   *
-   * It does not narrow the type of the record based on the specific record type. You should not be able, for example,
-   * to add a record of type 'A' with an answer that is an MxAnswer.
-   */
-  private data: Map<RecordType, SupportedAnswer[]> = new Map();
+export class DefaultStore extends EventEmitter implements Store {
+  public shouldCache: boolean;
+  constructor({ shouldCache = false }: { shouldCache?: boolean } = {}) {
+    super();
 
-  private _insert(labels: string[], rType: RecordType, data: SupportedAnswer[]) {
-    if (labels.length === 0) {
-      this.data.set(rType, data);
-      return;
+    this.shouldCache = shouldCache;
+    this.handler = this.handler.bind(this);
+  }
+  data: Map<string, TypedMap<SupportedRecordType, ZoneData>> = new Map();
+
+  set<T extends SupportedRecordType>(domain: string, rType: T, data: ZoneData[T] | ZoneData[T][]): void {
+    const record = this.data.get(domain) || (new Map() as TypedMap<SupportedRecordType, ZoneData>);
+    if (!Array.isArray(data)) {
+      data = [data];
     }
 
-    const [label, ...rest] = labels;
-    const isWildcard = label === '*';
-
-    if (isWildcard) {
-      // if the label is a wildcard, we should clear the labels array
-      labels = [];
-    }
-
-    let next = this.trie.get(label);
-
-    if (!next) {
-      next = new AnswerTrie();
-      this.trie.set(label, next);
-    }
-
-    next._insert(rest, rType, data);
+    record.set(rType, data);
+    this.data.set(domain, record);
   }
 
-  add(domain: string, rType: RecordType, data: SupportedAnswer[]) {
-    this._insert(this.domainToLabels(domain), rType, data);
-  }
-
-  private _getExact(labels: string[], rType?: RecordType): SupportedAnswer[] | null {
-    if (labels.length === 0) {
-      if (rType) {
-        return this.data.get(rType) || null;
+  get<T extends SupportedRecordType>(
+    domain: string,
+    rType?: T,
+    wildcards: boolean = true,
+  ): ZoneData[T][] | ZoneData[keyof ZoneData][] | null {
+    if (rType) {
+      const record = this.data.get(domain);
+      if (record && record.size > 0) {
+        return record.get(rType) || null;
       }
-
-      return Array.from(this.data.values()).flat();
+    } else {
+      const record = this.data.get(domain);
+      if (record && record.size > 0) {
+        return Array.from(record.values()).flat();
+      }
     }
 
-    const [label, ...rest] = labels;
-    const next = this.trie.get(label);
-
-    if (!next) {
+    if (!wildcards) {
       return null;
     }
 
-    return next._getExact(rest, rType);
-  }
+    let labels = domain.split('.').toSpliced(0, 0, '*');
 
-  private _get(labels: string[], rType?: RecordType): SupportedAnswer[] | null {
-    if (labels.length === 0) {
+    while (labels.length > 1) {
+      labels = labels.toSpliced(0, 2).toSpliced(0, 0, '*');
+
+      const wildcardDomain = labels.join('.');
+
       if (rType) {
-        return this.data.get(rType) || null;
-      }
-
-      return Array.from(this.data.values()).flat();
-    }
-
-    if (this.trie.has('*')) {
-      if (rType) {
-        const response = this.trie.get('*')!.data.get(rType);
-        if (!response) {
-          return null;
-        }
-        return response.map((record) => {
-          return {
-            ...record,
-            name: resolveWildcards(labels.toReversed().join('.'), record.name),
-          };
-        });
-      }
-      const response = Array.from(this.trie.get('*')!.data.values()).flat();
-      return response.map((record) => {
-        return {
-          ...record,
-          name: resolveWildcards(labels.toReversed().join('.'), record.name),
-        };
-      });
-    }
-
-    const [label, ...rest] = labels;
-    const next = this.trie.get(label);
-
-    if (!next) {
-      return null;
-    }
-
-    return next._get(rest, rType);
-  }
-
-  get(domain: string, rType?: RecordType): SupportedAnswer[] | null {
-    // first try to get the exact match
-    const exact = this._getExact(this.domainToLabels(domain), rType);
-    if (exact) {
-      return exact;
-    }
-
-    const result = this._get(this.domainToLabels(domain), rType);
-    if (!result || result.length === 0) {
-      return null;
-    }
-
-    return result;
-  }
-
-  private _has(labels: string[]): boolean {
-    if (labels.length === 0) {
-      return this.data.size > 0;
-    }
-
-    const [label, ...rest] = labels;
-    const next = this.trie.get(label);
-
-    if (!next) {
-      return false;
-    }
-
-    return next._has(rest);
-  }
-
-  has(domain: string): boolean {
-    return this._has(this.domainToLabels(domain));
-  }
-
-  private _delete(labels: string[], rType?: RecordType, rData?: SupportedAnswer) {
-    if (labels.length === 0) {
-      if (rType) {
-        if (!rData) {
-          this.data.delete(rType);
-          return;
-        }
-
-        const current = this.data.get(rType) || [];
-        const filtered = current.filter((record) => {
-          return JSON.stringify(record) !== JSON.stringify(rData);
-        });
-
-        if (filtered.length === 0) {
-          this.data.delete(rType);
-        } else {
-          this.data.set(rType, filtered);
-        }
-
-        // if no data is left, remove the record type
-        if (this.data.size === 0) {
-          this.trie.clear();
+        const record = this.data.get(wildcardDomain);
+        if (record && record.size > 0) {
+          return record.get(rType) || null;
         }
       } else {
-        this.data.clear();
+        const record = this.data.get(wildcardDomain);
+        if (record && record.size > 0) {
+          return Array.from(record.values()).flat();
+        }
       }
+    }
+
+    return null;
+  }
+
+  append<T extends SupportedRecordType>(domain: string, type: T, data: ZoneData[T]): void {
+    const record = this.data.get(domain) || (new Map() as TypedMap<SupportedRecordType, ZoneData>);
+    const existing = record.get(type) || [];
+    record.set(type, [...existing, data]);
+    this.data.set(domain, record);
+  }
+
+  delete<T extends SupportedRecordType>(domain: string, type?: T, data?: ZoneData[T]): void {
+    if (!type) {
+      this.data.delete(domain);
       return;
     }
 
-    const [label, ...rest] = labels;
-    const next = this.trie.get(label);
-
-    if (!next) {
+    const record = this.data.get(domain);
+    if (!record) {
       return;
     }
 
-    next._delete(rest, rType, rData);
-  }
-
-  delete(domain: string, rType?: RecordType, rData?: SupportedAnswer) {
-    this._delete(this.domainToLabels(domain), rType, rData);
-  }
-
-  append(domain: string, rType: RecordType, data: SupportedAnswer) {
-    const labels = this.domainToLabels(domain);
-    const [label, ...rest] = labels;
-    let next = this.trie.get(label);
-
-    if (!next) {
-      next = new AnswerTrie();
-      this.trie.set(label, next);
+    if (!data) {
+      record.delete(type);
+      return;
     }
 
-    if (rest.length === 0) {
-      const current = next.data.get(rType) || [];
-      next.data.set(rType, current.concat(data));
+    const existing = record.get(type);
+    if (!existing) {
+      return;
+    }
+
+    const newData = existing.filter((d) => d !== data);
+    if (newData.length === 0) {
+      record.delete(type);
+      return;
     } else {
-      next.append(rest.join('.'), rType, data);
+      record.set(type, newData);
     }
   }
 
-  private _resolve(labels: string[]): string {
-    if (labels.length === 0) {
-      return '';
-    }
-
-    const [label, ...rest] = labels;
-    const next = this.trie.get(label);
-
-    if (!next) {
-      return '';
-    }
-
-    if (next.trie.has('*')) {
-      return '*.' + label;
-    }
-
-    const resolved = next._resolve(rest);
-    if (resolved === '') {
-      return label;
-    }
-
-    return resolved + '.' + label;
-  }
-
-  resolve(domain: string): string {
-    return this._resolve(this.domainToLabels(domain));
-  }
-
-  private domainToLabels(domain: string): string[] {
-    return domain.split('.').reverse();
-  }
-
-  private serializeTrie(trie: Map<string, AnswerTrie>): object {
-    const obj: { [key: string]: object } = {};
-    for (const [key, value] of trie) {
-      obj[key] = {
-        data: Array.from(value.data.entries()),
-        trie: this.serializeTrie(value.trie),
-      };
-    }
-    return obj;
-  }
-
-  toString() {
-    const obj = this.serializeTrie(this.trie);
-    return JSON.stringify(obj);
-  }
-
-  private deserializeTrie(obj: DeserializedTrie) {
-    for (const [key, value] of Object.entries(obj)) {
-      const trie = new AnswerTrie();
-      trie.deserializeTrie(value.trie);
-      this.trie.set(key, trie);
-
-      for (const [rType, data] of value.data) {
-        trie.data.set(rType as RecordType, data);
-      }
-    }
-  }
-
-  static fromString(str: string): AnswerTrie {
-    const obj = JSON.parse(str) as DeserializedTrie;
-    const trie = new AnswerTrie();
-    trie.deserializeTrie(obj);
-    return trie;
-  }
-}
-
-/**
- * A simple in-memory store for storing DNS records.
- */
-export class DefaultStore extends EventEmitter implements Store {
-  trie: AnswerTrie = new AnswerTrie();
-
-  async get(zone: string, rType?: RecordType): Promise<SupportedAnswer | SupportedAnswer[] | null> {
-    return this.trie.get(zone, rType);
-  }
-
-  async set(zone: string, rType: RecordType, data: SupportedAnswer | SupportedAnswer[]): Promise<void> {
-    this.trie.add(zone, rType, Array.isArray(data) ? data : [data]);
-  }
-
-  async append(zone: string, rType: RecordType, data: SupportedAnswer): Promise<void> {
-    this.trie.append(zone, rType, data);
-  }
-
-  async delete(zone: string, rType?: RecordType, rData?: SupportedAnswer): Promise<void> {
-    this.trie.delete(zone, rType, rData);
-  }
-
-  handler: Handler = async (req: DNSRequest, res: DNSResponse, next: NextFunction) => {
+  handler(req: DNSRequest, res: DNSResponse, next: NextFunction) {
     const { questions } = req.packet;
     const { name, type } = questions![0];
 
-    const records = this.trie.get(name, type);
+    const records = this.get(name, type);
 
-    if (records && records.length > 0) {
-      res.answer(records);
-      // this.emit('cacheRequest', {
-      //     zoneName: this.trie.resolve(name),
-      //     recordType: type,
-      //     records: records
-      // });
+    const answers: SupportedAnswer[] | undefined = records?.map((record) => {
+      return {
+        name,
+        type,
+        data: record,
+      } as SupportedAnswer;
+    });
+
+    if (answers && answers.length > 0) {
+      res.answer(answers);
+
+      if (this.shouldCache) {
+        this.emitCacheRequest(name, type, records as ZoneData[SupportedRecordType][]);
+      }
     }
 
     next();
-  };
+  }
+
+  async emitCacheRequest<T extends SupportedRecordType>(zone: string, rType: T, records: ZoneData[T][]) {
+    this.emit('cacheRequest', {
+      zoneName: zone,
+      recordType: rType,
+      records: records,
+    });
+  }
+
+  toString() {
+    return JSON.stringify(
+      Object.fromEntries(
+        Array.from(this.data.entries()).map(([domain, records]) => {
+          return [
+            domain,
+            Object.fromEntries(
+              Array.from(records.entries()).map(([rType, data]) => {
+                return [rType, data];
+              }),
+            ),
+          ];
+        }),
+      ),
+    );
+  }
+
+  static fromString(str: string) {
+    const obj = JSON.parse(str) as { [key: string]: { [key in SupportedRecordType]: ZoneData[key][] } };
+    const store = new DefaultStore();
+
+    for (const [domain, data] of Object.entries(obj)) {
+      for (const [rType, records] of Object.entries(data)) {
+        store.set(domain, rType as SupportedRecordType, records);
+      }
+    }
+
+    return store;
+  }
 }
--- src/plugins/storage/Store.ts
+++ src/plugins/storage/Store.ts
@@ -1,13 +1,18 @@
 import { Handler } from '../../common/server';
-import { Answer, RecordType } from 'dns-packet';
+import { ZoneData, SupportedRecordType } from '../../types/dns';
+import { EventEmitter } from 'events';
+import { Awaitable } from '../../common/core/utils';
 
-export abstract class Store {
+export abstract class Store extends EventEmitter {
   /**
    * Retrieve information about a zone in the database.
    * @param zone The name of the zone to retrieve
    * @param rType The record type to retrieve. If not provided, all records in the zone should be retrieved.
    */
-  abstract get(zone: string, rType?: RecordType): Promise<Answer | Answer[] | null>;
+  abstract get<T extends SupportedRecordType>(
+    zone: string,
+    rType?: T,
+  ): Awaitable<ZoneData[T][] | ZoneData[keyof ZoneData][] | null>;
 
   /**
    * Set or update information about a zone in the database.
@@ -15,7 +20,11 @@ export abstract class Store {
    * @param rType The record type to set
    * @param data The data to set
    */
-  abstract set(zone: string, rType: RecordType, data: Answer | Answer[]): Promise<void>;
+  abstract set<T extends SupportedRecordType>(
+    zone: string,
+    rType: T,
+    data: ZoneData[T] | ZoneData[T][],
+  ): Awaitable<void>;
 
   /**
    * Append information about a zone in the database.
@@ -23,7 +32,7 @@ export abstract class Store {
    * @param rType The record type to append
    * @param data The data to append
    */
-  abstract append(zone: string, rType: RecordType, data: Answer): Promise<void>;
+  abstract append<T extends SupportedRecordType>(zone: string, rType: T, data: ZoneData[T]): Awaitable<void>;
 
   /**
    *
@@ -31,7 +40,7 @@ export abstract class Store {
    * @param rType the record type to delete. If not provided, all records in the zone should be deleted.
    * @param data the data to delete. If not provided, all records of the given type should be deleted.
    */
-  abstract delete(zone: string, rType?: RecordType, rData?: Answer): Promise<void>;
+  abstract delete<T extends SupportedRecordType>(zone: string, rType?: T, rData?: ZoneData[T]): Awaitable<void>;
 
   abstract handler: Handler;
 }
