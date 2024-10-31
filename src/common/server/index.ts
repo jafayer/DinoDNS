e5e652d88b7b5db4717466e05e2d756f89cdc957
jafayer <github@fayer.me>
2024-10-31 08:56:30 -0400

    Move types to explicit export

--- src/common/server/index.ts
+++ src/common/server/index.ts
@@ -1,2 +1,2 @@
 export { DNSServer, DefaultServer, DefaultServerProps } from './DefaultServer';
-export { DNSRequest, DNSResponse, Handler, NextFunction } from '../../types/server';
+
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * as common from './common';
 export * as plugins from './plugins';
+export * as types from './types';
