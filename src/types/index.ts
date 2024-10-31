de1d7b01c4ac1fa66fd1ef76587431d87c1165ee
jafayer <github@fayer.me>
2024-10-31 09:36:54 -0400

    Explicitly export all values from types for now

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,13 @@
-export * from './dns';
-export * from './server';
+export { SupportedAnswer, SupportedQuestion, SupportedRecordType, ZoneData } from './dns';
+export {
+  CanAnswer,
+  DNSRequest,
+  DNSResponse,
+  Handler,
+  NextFunction,
+  DuplicateAnswerForRequest,
+  MessageMetadata,
+  ModifiedAfterSentError,
+  PacketWrapper,
+  Timings,
+} from './server';
