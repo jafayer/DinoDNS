555659e90b77dfa935314a5aecd5c298f149f518
jafayer <github@fayer.me>
2024-11-03 11:33:19 -0500

    Implement DNS over HTTP and HTTPS in the helloWorld example

--- src/examples/helloWorld/index.ts
+++ src/examples/helloWorld/index.ts
@@ -2,6 +2,7 @@ import { ConsoleLogger } from '../../plugins/loggers';
 import { DefaultStore } from '../../plugins/storage';
 import { DNSOverTCP, DNSOverUDP, DefaultServer } from '../../common';
 import { DNSOverHTTP } from '../../common/network/doh';
+import fs from "fs";
 
 const logger = new ConsoleLogger(true, true);
 
@@ -14,7 +15,12 @@ const s = new DefaultServer({
     new DNSOverUDP('localhost', 1054),
     new DNSOverHTTP({
       httpPort: 1080,
+      httpsPort: 1443,
       address: 'localhost',
+      ssl: {
+        cert: fs.readFileSync('./ssl/cert.crt'), // obviously don't use the provided TLS certificate in prod
+        key: fs.readFileSync('./ssl/key.pem'), // also this may expire in the future, so this demo might break
+      }
     }),
   ],
 });
@@ -23,5 +29,5 @@ s.use(logger.handler.bind(logger));
 s.use(st.handler);
 
 s.start(() => {
-  console.log('Server started');
+  console.log('Server started, using protocols:' + s.networks.map(n => n.networkType).join(', '));
 });
